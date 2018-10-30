0f648e3b134c4869e71a8c89316231f57c95d794
Rachel Wong <rwrachelwong@gmail.com>
2018-10-30 02:38:25 +0000

    included a function definition

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,14 @@
 var request = require('request');
 
+console.log('---------------------------------------');
 console.log('Welcome to the Github Avatar Downloader');
+console.log('---------------------------------------');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
