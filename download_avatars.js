254b154125749f52bcba73602279dc5431691375
Rachel Wong <rwrachelwong@gmail.com>
2018-10-30 02:58:21 +0000

    added a .gitignore file

--- download_avatars.js
+++ download_avatars.js
@@ -1,11 +1,23 @@
 var request = require('request');
+var token = require('./secrets');
 
 console.log('---------------------------------------');
 console.log('Welcome to the Github Avatar Downloader');
 console.log('---------------------------------------');
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    Authorization: 'token',
+    headers: {
+      'User-Agent': 'request'
 
+    }
+  };
+
+  request(url, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
